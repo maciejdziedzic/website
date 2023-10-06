70022216d04f3309faaa45d81c179cae7439b1d3
maciejdziedzic <58685221+maciejdziedzic@users.noreply.github.com>
2023-10-06 12:06:20 +0200

    add few pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import Sun from "../../assets/sun.svg";
 import Moon from "../../assets/moon.svg";
 import useDarkMode from "../../contexts/Darkmode/useDarkMode";
 
 export default function Navbar() {
   const { darkMode, toggledarkMode } = useDarkMode();
+  const [ProjectsOpen, setProjectsOpen] = useState(false);
+
+  const toggleProjects = () => {
+    setProjectsOpen(!ProjectsOpen);
+  };
 
   return (
-    <div className="flex h-16">
+    <div className="flex h-16 ml-32 mr-32">
       <div
         className={
           darkMode === "dark"
@@ -15,9 +22,13 @@ export default function Navbar() {
         }
       >
         <ul className="flex items-center justify-center space-x-36 tracking-widest text-lg h-full">
-          <li>Home</li>
-          <li>Projects</li>
-          <li>About</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li onClick={toggleProjects}>Projects</li>
           <li>
             <img
               src={darkMode === "dark" ? Sun : Moon}
@@ -27,6 +38,21 @@ export default function Navbar() {
             />
           </li>
         </ul>
+        {ProjectsOpen && (
+          <div
+            className={
+              darkMode === "dark"
+                ? "bg-red-100 bg-opacity-20 h-16 items-center flex justify-center"
+                : "bg-gray-300  h-16 items-center flex justify-center"
+            }
+          >
+            <ul className="flex space-x-16 justify-center text-lg tracking-widest">
+              <li>Assets Return</li>
+              <li>Macroeconomics Chart</li>
+              <li>Recession Model</li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
