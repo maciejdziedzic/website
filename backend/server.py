636db40ea25df4cf22eca039c13064f0231be89a
maciejdziedzic <58685221+maciejdziedzic@users.noreply.github.com>
2023-10-09 12:43:12 +0200

    create python env

--- backend/server.py
+++ backend/server.py
@@ -1,1 +1,10 @@
-# hello world
+from flask import Flask, jsonify
+from flask_cors import CORS
+from pymongo import MongoClient
+
+app = Flask(__name__)
+CORS(app)
+
+client = MongoClient('mongodb://127.0.0.1:27017/')
+
+db = client['chartData']
