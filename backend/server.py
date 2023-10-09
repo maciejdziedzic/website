cf4ef93a0bc718f983f21f8b4df8b62b183165c2
maciejdziedzic <58685221+maciejdziedzic@users.noreply.github.com>
2023-10-09 14:07:18 +0200

    flask python server check

--- backend/server.py
+++ backend/server.py
@@ -8,3 +8,14 @@ CORS(app)
 client = MongoClient('mongodb://127.0.0.1:27017/')
 
 db = client['chartData']
+
+
+@app.route('/add_data', methods=['GET'])
+def add_data():
+    sample_data = {"name": "example", "value": 100}
+    db.sample_collection.insert_one(sample_data)
+    return jsonify({"message": "Data added"}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
