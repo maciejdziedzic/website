c4afdafa5bfbbe5b6eb67f935960d16cb5cb8a5b
maciejdziedzic <58685221+maciejdziedzic@users.noreply.github.com>
2023-10-10 15:09:27 +0200

    before changing data from mongodb

--- backend/data.py
+++ backend/data.py
@@ -10,18 +10,3 @@ FRED_API_KEY = os.getenv("API_KEY")
 fred = Fred(api_key=FRED_API_KEY)
 client = MongoClient('mongodb://127.0.0.1:27017/')
 db = client['chartData']
-
-
-def fetch_and_store_data():
-    start_date = datetime.datetime(1970, 1, 1)
-    end_date = datetime.datetime(2022, 12, 31)
-    __bonds2tr = fred.get_series('DGS2', start=start_date, end=end_date)
-    # Convert the Timestamp keys to string
-    data_to_store = {str(k): v for k, v in __bonds2tr.to_dict().items()}
-    print(data_to_store)
-    # Here, you may decide on how you want to structure the data in MongoDB.
-
-    db.bonds2tr.insert_one({"data": data_to_store})
-
-
-fetch_and_store_data()
--- backend/server.py
+++ backend/server.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 CORS(app)
 
 client = MongoClient('mongodb://127.0.0.1:27017/')
-db = client['chartData']
+db = client['economic_data']
 
 
 @app.route('/get_data', methods=['GET'])
 def get_data():
-    # Query data from MongoDB and send it as JSON.
-    # Omitting MongoDB _id for JSON serialization
-    data = db.bonds2tr.find_one({}, {"_id": 0})
+    collection = db['economic_collection']
+    data = list(collection.find({}, {'_id': False}))
+    print(data)
     return jsonify(data)
 
 
