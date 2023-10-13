ff6b68aaa628ed1e8995f38b029913c993da93a7
maciejdziedzic <58685221+maciejdziedzic@users.noreply.github.com>
2023-10-13 20:56:37 +0200

    model works

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request
 from services.fetch_data import fetch_data
-# from model.run_model import run_model
+import pandas as pd
+from model.run_model import run_model
+import joblib
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -18,29 +20,28 @@ def get_data():
         return jsonify(error=str(e)), 500
 
 
+logistic_regression = joblib.load('data/saved_model.pkl')
+
+
 @api_blueprint.route('/run-model', methods=['POST'])
 def predict():
-    data = request.get_json()
 
     try:
-
-        # return 'helo kurwa'
-        # Get JSON data
         data = request.get_json()
+        print(data)
         # # Extract gdp and iyc values
         gdp = data.get('gdp')
         iyc = data.get('iyc')
-        # print(jsonify(data))
-        return jsonify(data)
 
         # # Run model
-        # feature_values = [gdp, iyc]
-        # These should be the actual feature names used during training
-        # feature_names = ['gdp', 'iyc']
-        # X_test = pd.DataFrame([feature_values], columns=feature_names)
-        # pred_test = logistic_regression.predict(X_test)
+        feature_values = [gdp, iyc]
+        feature_names = ['gdp', 'iyc']
+        X_test = pd.DataFrame([feature_values], columns=feature_names)
 
+        pred_test = logistic_regression.predict(X_test)
+        print(type(str(pred_test)))
         # # Return prediction as JSON
+        return jsonify(str(pred_test))
         # return jsonify(prediction=pred_test.tolist())
 
     except Exception as e:
--- backend/model/run_model.py
+++ backend/model/run_model.py
@@ -1,8 +1,8 @@
-# import joblib
-# import numpy as np
-# import pandas as pd
+import joblib
+import numpy as np
+import pandas as pd
 
-# logistic_regression = joblib.load('data/saved_model.pkl')
+
 # feature_values = [0.05,  0.4]
 # feature_names = ['gdp', 'iyc']
 
@@ -10,7 +10,9 @@
 # pred_test = logistic_regression.predict(X_test)
 
 # print(pred_test)
-# def run_model():
-#     log_msg = 'hello model'
-#     print(log_msg)
-#     return log_msg
+
+
+def run_model():
+    logistic_regression = joblib.load('data/saved_model.pkl')
+
+    return log_msg
