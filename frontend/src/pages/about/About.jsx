5a97291abe960e2b2187ea6afa49f221717f45e8
maciejdziedzic <58685221+maciejdziedzic@users.noreply.github.com>
2023-11-02 07:04:25 +0100

    good morning

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
 
   return (
     <div
-      className={`container mx-auto mt-10 p-5 ${
+      className={`container mx-auto m-5 p-5 ${
         darkMode ? "bg-neutral-700 text-white" : "bg-white text-neutral-700"
       }`}
     >
@@ -14,20 +14,10 @@ export default function About() {
       <section className="mb-5">
         <h2 className="text-xl font-semibold mb-3">Introduction</h2>
         <p>
-          Welcome to [Your Website Name], a platform dedicated to showcasing
-          intricate data visualizations and in-depth analysis of key financial
-          metrics. Our aim is to provide insights and perspectives that are both
-          informative and thought-provoking.
-        </p>
-      </section>
-
-      <section className="mb-5">
-        <h2 className="text-xl font-semibold mb-3">Motives</h2>
-        <p>
-          The driving force behind this website is a passion for data analytics
-          and a curiosity to uncover patterns and insights in the financial
-          world. It&apos;s an endeavor to make complex data more accessible and
-          understandable to a wider audience.
+          I created this website to deepen my understanding of web design,
+          statistics and economics, and to showcase my research to an audiance,
+          who in my hope will find it interesting and may be interested to
+          collaborate with me on a project.
         </p>
       </section>
 
@@ -46,9 +36,8 @@ export default function About() {
         <h2 className="text-xl font-semibold mb-3">Data Overview</h2>
         <p>
           The data showcased here is curated from reputable sources and is
-          processed to ensure accuracy and relevance. From macroeconomic
-          indicators to asset returns, we aim to provide a comprehensive look at
-          the financial landscape.
+          processed to ensure accuracy and relevance. If you find any errors,
+          please let me know by e-mail at: maciej.dziedzic9@gmail.com
         </p>
       </section>
     </div>
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -8,16 +8,22 @@ function Home() {
   const { darkMode } = useContext(DarkModeContext);
   const [part1, setPart1] = useState("");
   const [part2, setPart2] = useState("");
+  const [part3, setPart3] = useState("");
+  const [part4, setPart4] = useState("");
 
-  const text1 = "Hello, welcome to my website. ";
-  const text2 = "You can find here three projects:\n\n";
+  const text1 =
+    "Hello, welcome to my website.\nYou can find here three projects: ";
+  const text2 =
+    "Assets Returns - yearly returns from different assets across 1970-2022";
+  const text3 = "Macroeconomic Charts - macroeconomic variables charts across";
+  const text4 = "Recession model - model that predicts FED's policy";
 
   useEffect(() => {
     if (part1.length < text1.length) {
       const timer1 = setTimeout(() => {
         const nextChar1 = text1[part1.length];
         setPart1((prev) => prev + nextChar1);
-      }, 15);
+      }, 10);
 
       return () => clearTimeout(timer1);
     } else if (part2.length < text2.length) {
@@ -27,35 +33,38 @@ function Home() {
       }, 10);
 
       return () => clearTimeout(timer2);
+    } else if (part3.length < text3.length) {
+      const timer3 = setTimeout(() => {
+        const nextChar3 = text3[part3.length];
+        setPart3((prev) => prev + nextChar3);
+      }, 10);
+
+      return () => clearTimeout(timer3);
+    } else if (part4.length < text4.length) {
+      const timer4 = setTimeout(() => {
+        const nextChar4 = text4[part4.length];
+        setPart4((prev) => prev + nextChar4);
+      }, 10);
+
+      return () => clearTimeout(timer4);
     }
-  }, [part1, part2]);
+  }, [part1, part2, part3, part4]);
 
   return (
-    <div className="flex items-center justify-center m-10">
+    <div className="flex items-center justify-center  m-10">
       <div
-        className={`items-center justify-center ${
+        className={`items-center   ${
           darkMode ? "text-dark-text " : "text-white-100"
         }`}
       >
-        <div className="text-container">
-          <span>{part1}</span>
+        <div className="text-container font-thin">
+          <p>{part1}</p>
           <br />
-          <pre>{part2}</pre>
-          <Link to="/project1">
-            Assets Returns - yearly returns from different assets across
-            1970-2022
-          </Link>
+          <Link to="/project1">{part2}</Link>
           <br />
-          <Link to="/project2">
-            Macroeconomic Charts - chosen variables that can be visualised in
-            one chart
-          </Link>
+          <Link to="/project2">{part3}</Link>
           <br />
-          <Link to="/project3">
-            Recession model - model that predicts SP500 future return, based on
-            web scraped data and chat gpt interpretation of FED&apos;s official
-            communication
-          </Link>
+          <Link to="/project3">{part4}</Link>
 
           <br />
           <span className="cursor">_</span>
