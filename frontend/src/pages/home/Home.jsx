56a361a3162843d4cf4001da5f6f6d460228b23b
maciejdziedzic <58685221+maciejdziedzic@users.noreply.github.com>
2023-11-02 16:35:14 +0100

    add data and model sections

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -12,6 +12,11 @@ function Home() {
   const [part3, setPart3] = useState("");
   const [recessionModel, setRecessionModel] = useState("");
   const [part4, setPart4] = useState("");
+  const [data, setData] = useState("");
+  const [part5, setPart5] = useState("");
+  const [model, setModel] = useState("");
+  const [part6, setPart6] = useState("");
+
   const [allTextPrinted, setAllTextPrinted] = useState(false);
 
   const text1 =
@@ -22,13 +27,17 @@ function Home() {
   const text3 = " U.S. Economic Indicators ";
   const textRecessionModel = "Recession model";
   const text4 = " Model that Predicts future FED's Policy";
+  const textData = "Data";
+  const textPart5 = " Sources and transformations used in all projects";
+  const textModel = "Model";
+  const textPart6 = " Additional information on used model";
 
   useEffect(() => {
     let timer;
     if (part1.length < text1.length) {
       timer = setTimeout(
         () => setPart1((prev) => prev + text1[part1.length]),
-        8
+        6
       );
     } else if (assetsReturns.length < textAssetsReturns.length) {
       timer = setTimeout(
@@ -36,23 +45,23 @@ function Home() {
           setAssetsReturns(
             (prev) => prev + textAssetsReturns[assetsReturns.length]
           ),
-        7
+        5
       );
     } else if (part2.length < text2.length) {
       timer = setTimeout(
         () => setPart2((prev) => prev + text2[part2.length]),
-        7
+        5
       );
     } else if (macroCharts.length < textMacroCharts.length) {
       timer = setTimeout(
         () =>
           setMacroCharts((prev) => prev + textMacroCharts[macroCharts.length]),
-        7
+        5
       );
     } else if (part3.length < text3.length) {
       timer = setTimeout(
         () => setPart3((prev) => prev + text3[part3.length]),
-        7
+        5
       );
     } else if (recessionModel.length < textRecessionModel.length) {
       timer = setTimeout(
@@ -60,18 +69,50 @@ function Home() {
           setRecessionModel(
             (prev) => prev + textRecessionModel[recessionModel.length]
           ),
-        7
+        5
       );
     } else if (part4.length < text4.length) {
       timer = setTimeout(
         () => setPart4((prev) => prev + text4[part4.length]),
-        7
+        5
+      );
+    } else if (data.length < textData.length) {
+      timer = setTimeout(
+        () => setData((prev) => prev + textData[data.length]),
+        5
+      );
+    } else if (part5.length < textPart5.length) {
+      timer = setTimeout(
+        () => setPart5((prev) => prev + textPart5[part5.length]),
+        5
+      );
+    } else if (model.length < textModel.length) {
+      timer = setTimeout(
+        () => setModel((prev) => prev + textModel[model.length]),
+        5
+      );
+    } else if (part6.length < textPart6.length) {
+      timer = setTimeout(
+        () => setPart6((prev) => prev + textPart6[part6.length]),
+        5
       );
     } else {
       setAllTextPrinted(true);
     }
     return () => clearTimeout(timer);
-  }, [part1, assetsReturns, part2, macroCharts, part3, recessionModel, part4]);
+  }, [
+    part1,
+    assetsReturns,
+    part2,
+    macroCharts,
+    part3,
+    recessionModel,
+    part4,
+    data,
+    part5,
+    model,
+    part6,
+  ]);
 
   return (
     <div className="flex items-center justify-center my-24 mx-auto">
@@ -101,6 +142,20 @@ function Home() {
                   {recessionModel}
                 </Link>
                 <span className="ml-2 description">{part4}</span>
+              </div>{" "}
+              <br />
+              <div className="flex items-center my-2">
+                <Link to="/data" className="flex-1 project-button ">
+                  {data}
+                </Link>
+                <span className="ml-2 description">{part5}</span>
+              </div>
+              <div className="flex items-center my-2">
+                <Link to="/model" className="flex-1 project-button ">
+                  {model}
+                </Link>
+                <span className="ml-2 description">{part6}</span>
+
                 {allTextPrinted && <span className="cursor">_</span>}
               </div>
             </div>
