9faaab50facc4824bacd45167c1d3b0ed50452b0
maciejdziedzic <58685221+maciejdziedzic@users.noreply.github.com>
2024-06-04 00:06:59 +0200

    smooth curves

--- backend/data/save_to_mongodb.py
+++ backend/data/save_to_mongodb.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import datetime
 from fredapi import Fred
 
-env_path = pathlib.Path(__file__).parent.parent / '.env'
+env_path = pathlib.Path('..') / '.env'
 load_dotenv(dotenv_path=env_path)
 FRED_API_KEY = os.getenv("FRED_API_KEY")
 fred = Fred(api_key=FRED_API_KEY)
@@ -16,6 +16,7 @@ client = MongoClient('localhost', 27017)
 db = client.project2_db
 collection = db.project2_collection
 
+# Define your series dictionary, start and end dates as before
 series_dict = {
     'bonds2tr': ('DGS2', 'D'),
     'gdpworld': ('NYGDPMKTPCDWLD', 'A'),
@@ -37,7 +38,7 @@ series_dict = {
     'wages': ('AHETPI', 'M'),
     'cp': ('CP', 'Q'),
     'fed_debt_to_gdp': ('GFDGDPA188S', 'A'),
-    'comm_banks_borrowings': ('LOANINV', 'M')
+    'comm_banks_borrowings': ('LOANINV', 'D')
 }
 
 # Initialize DataFrames
@@ -59,36 +60,12 @@ df = {}
 df['D_M'] = dfs['D'].resample('M').last()
 df['M_M'] = dfs['M'].resample('M').last()
 df['Q_M'] = dfs['Q'].resample('M').ffill()
-df['A_M'] = dfs['A'].resample('M').ffill()
 # new_dates = pd.date_range(
 #     start=df['Q_M'].index.min(), end='2022-12-31', freq='M')
 # df['Q_M'] = df['Q_M'].reindex(new_dates).ffill()
-# df['A_M'] = dfs['A'].resample('M').ffill()
+df['A_M'] = dfs['A'].resample('M').ffill()
 
 data = pd.concat([df['D_M'], df['M_M'], df['Q_M'], df['A_M']], axis=1)
-
-data['wages_month'] = round(data['wages'] * 168, 2)
-data['house_wages'] = round(data['houses']/data['wages_month'], 2)
-data['iyc'] = round(data['bonds10tr'] - data['bonds2tr'], 2)
-data['gdp_pct'] = round(data['gdp'].pct_change(periods=4) * 100, 2)
-data['gdp_pct_ma4'] = round(data['gdp_pct'].rolling(window=12).mean(), 2)
-data['gdpworld_pct'] = round(data['gdpworld'].pct_change(12) * 100, 2)
-# data['debt_to_gdp'] = round(100 * data['debt'] / data['gdp']/1000, 2)
-data['debt_to_gdp'] = round(data['fed_debt_to_gdp'], 2)
-data['resins_to_gdp'] = round(100 * data['resins'] / data['gdp'], 2)
-data['cbasstogdp'] = round(data['cbasstogdp'], 2)
-data['cp_to_gdp'] = round(100 * data['cp'] / data['gdp'], 2)
-data['m0_to_gdp'] = round(data['m0'] / data['gdp']/1000 * 100, 2)
-data['m3_to_gdp'] = round((data['m3']/1000000) / data['gdp']/1000 * 100, 2)
-# data['cpi_pct'] = round(data['cpi'].pct_change(periods=12) * 100, 2)
-data['ppi_pct'] = round(data['ppi'].pct_change(periods=12) * 100, 2)
-data['indpro'] = round(data['indpro'], 2)
-data['oil'] = round(data['oil'], 2)
-data['comm_banks_to_gdp'] = round(
-    100 * (data['comm_banks_borrowings'] / (data['gdp'])), 2)
-
-base_year = 258.906
-data['adjusted_oil'] = data['oil'] * (base_year / data['cpi'])
 
 csv_file_path = '../../frontend/public/cpi_manual_data.csv'
 df = pd.read_csv(csv_file_path)
@@ -114,6 +91,33 @@ merged_df = data.merge(df_melted['cpi_manual'], on='date', how='left')
 
 merged_df.index.name = 'date'
 data = merged_df.reset_index()
+
+data['wages_month'] = round(data['wages'] * 168, 2)
+data['house_wages'] = round(data['houses']/data['wages_month'], 2)
+data['iyc'] = round(data['bonds10tr'] - data['bonds2tr'], 2)
+data['gdp_pct'] = round(data['gdp'].pct_change(periods=4) * 100, 2)
+data['gdp_pct_ma4'] = round(data['gdp_pct'].rolling(window=12).mean(), 2)
+data['cpi_pct'] = round(data['cpi'].pct_change(periods=12) * 100, 2)
+data['ppi_pct'] = round(data['ppi'].pct_change(periods=12) * 100, 2)
+data['ppi_pct'] = round(data['ppi_pct'].rolling(window=6).mean(), 2)
+data['cpi_manual'] = round(data['cpi_manual'].rolling(window=6).mean(), 2)
+data['fedrate'] = round(data['fedrate'].rolling(window=6).mean(), 2)
+data['gdpworld_pct'] = round(data['gdpworld'].pct_change(12) * 100, 2)
+# data['debt_to_gdp'] = round(100 * data['debt'] / data['gdp']/1000, 2)
+data['debt_to_gdp'] = round(data['fed_debt_to_gdp'], 2)
+data['resins_to_gdp'] = round(100 * data['resins'] / data['gdp'], 2)
+data['cp_to_gdp'] = round(100 * data['cp'] / data['gdp'], 2)
+data['m0_to_gdp'] = round(data['m0'] / data['gdp']/1000 * 100, 2)
+data['m3_to_gdp'] = round((data['m3']/1000000) / data['gdp']/1000 * 100, 2)
+data['cbasstogdp'] = round(data['cbasstogdp'], 2)
+data['indpro'] = round(data['indpro'], 2)
+data['oil'] = round(data['oil'], 2)
+data['comm_banks_to_gdp'] = round(
+    100 * (data['comm_banks_borrowings'] / (data['gdp'])), 2)
+# 01.2020
+base_year = 258.906
+data['adjusted_oil'] = data['oil'] * (base_year / data['cpi'])
+
 
 data_to_insert = data.to_dict(orient='records')
 collection.insert_many(data_to_insert)
